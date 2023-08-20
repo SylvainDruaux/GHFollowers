e23b8c4b2251f6622a67faadbe4c21b38058dc90
SylvainDruaux <sydru06@gmail.com>
2023-08-20 20:31:57 +0200

    Initial Commit

---
+++ GHFolowers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GHFolowers
+//
+//  Created by Sylvain Druaux on 20/08/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
